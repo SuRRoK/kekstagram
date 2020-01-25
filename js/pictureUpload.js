4014155bfbf945e00738ba10ac829a71d1b7f718
BoomerOK <surrok6@gmail.com>
2020-01-25 21:49:00 +0300

    Урок 4. Все функции для проверки Хеш-тегов написаны

--- js/pictureUpload.js
+++ js/pictureUpload.js
@@ -23,17 +23,27 @@ uploadSubmit.addEventListener('click', function (evt) {
   setCustomValidity();
 });
 
+
 function removeSpaces(string) {
   string = string.replace(/\s+/g, ' ');
-  if (string[0] === ' ') {
+  if (string.startsWith(' ') || string.endsWith(' ')) {
     var array = string.split('');
-    array.splice(0, 1);
+    if (array[0] === ' ') {
+      array.splice(0, 1);
+    }
     var lastElem = array.length - 1;
     if (array[lastElem] === ' ') {
       array.splice(lastElem, 1);
     }
     string = array.join('');
   }
+  return string;
+}
+
+function removeSpacesNew(string) {
+  var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g
+  string = string.replace(/\s+/g, ' ');
+  string = string.replace(rtrim, '');
 
   return string;
 }
@@ -47,14 +57,37 @@ function startSymbol(symbol, words) {
   return true;
 }
 
+function secondEntrySymbol(symbol, words) {
+  for (var i = 0; i < words.length; i++) {
+    if (words.includes(symbol, 1)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function wordLength(length, words) {
+  for (var i = 0; i < words.length; i++) {
+    if (words[i] > length) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function wordDuplicate(words) {
+  // eslint-disable-next-line no-undef
+  var wordsSet = new Set(words);
+  return wordsSet.size === words.length;
+}
 
 function setCustomValidity() {
-  var hashTags = removeSpaces(hashTagsInput.value);
-  hashTags = hashTags.split(' ');
-
-  for (var i = 0; i < hashTags.length; i++) {
-    startSymbol('#', hashTags[i]);
-  }
+  var hashTags = removeSpacesNew(hashTagsInput.value.toLowerCase()).split(' ');
+  startSymbol('#', hashTags);
+  secondEntrySymbol('#', hashTags);
+  wordLength(20, hashTags);
+  wordDuplicate(hashTags);
+  console.log(hashTags);
 }
 
 
